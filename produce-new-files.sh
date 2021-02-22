0f8088dfe8bd3f6b643e322496df1fdd139c09a3
Concourse Tests <concourse-tests@protonmail.com>
2021-02-22 05:12:16 -0600

    Fix typo

--- produce-new-files.sh
+++ produce-new-files.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
-# Save environment in a .env file
-env > new-directory/.env
+# Directory new-directory will be created automatically
+
+# Save environment in an env file
+env > new-directory/env
 
 # Save current date/time in TIMESTAMP file
 date > new-directory/TIMESTAMP
 
 # List current files on the scripts directory
-ls -la sample-scripts > ew-directory/FILELIST
+ls -la sample-scripts > new-directory/FILELIST
 
 echo 'New directory populated'
+ls -la new-directory
 
